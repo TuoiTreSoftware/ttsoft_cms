254b71633e0f7b8a25dd289c2d01cad571ee093f
Phúc HP <wolf.alone613@gmail.com>
2019-12-11 13:46:30 +0700

    fix

--- packages/Frontend/Resources/views/documents/email.blade.php
+++ packages/Frontend/Resources/views/documents/email.blade.php
@@ -7,7 +7,13 @@
 <body>
 <section class="content">
 	<h2>Hello {{ $name }}!</h2>
+	<h5>{{ $subject }}</h5>
+	@if($subject === 'elite')
 	<a href="https://drive.google.com/file/d/1preODoBUKOaYDICDNZDT5Bzl5hZsL7Tr/view?usp=sharing">Get Our Template Admin</a>
+	@endif
+	@if($subject === 'canvas')
+	<a href="https://drive.google.com/open?id=19CJlbcTDWAhDSA6XaREe9MGUl6_qJS_Y">Get Our Template Frontend</a>
+	@endif
 </section>
 </body>
 </html>
